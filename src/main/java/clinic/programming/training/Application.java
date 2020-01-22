2e9dc70c9c75da6b684aff7b51b7d4127746be3d
Amarpreet Sandhu <Amarpreet.sandhu@sqa-consulting.com>
2020-01-22 14:56:47 +0000

    Changing complier plugin to Java source 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,18 +1,25 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Application {
 
-    
+    public void greet()	{
 
-    public Application() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for (String greeting : greetings){
+			System.out.println("Greeting: " + greeting);
+		}
+	}
 
+ public Application() {
         System.out.println ("Inside Application");
 
     }
-
-
 
     // method main(): ALWAYS the APPLICATION entry point
 
@@ -21,6 +28,7 @@ public class Application {
     	System.out.println ("Starting Application");
 
 	Application app = new Application();
+	app.greet();
 
     }
 
